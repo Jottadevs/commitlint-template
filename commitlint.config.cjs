02d78db2885415a57d03dca158ee79156166d23a
João <joaofuscaalencar@gmal.com>
2025-11-12 10:50:08 -0300

    fix: adição de novos tipos de conventional

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -4,6 +4,28 @@ module.exports = {
   rules: {
     'header-max-length': [2, 'always', 72],
     'subject-case': [2, 'always', ['sentence-case', 'lower-case']],
-    'subject-full-stop': [2, 'never', ['.']]
+    'subject-full-stop': [2, 'never', ['.']],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+        // 👇 seus tipos personalizados:
+        'update',
+        'config',
+        'merge',
+        'create'
+      ]
+    ]
   }
 };
